a7059e7f0ecd52bd58cc79b99c739a85261e327c
Hazrat Ali <hazratali@users.noreply.github.com>
2025-07-17 15:22:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// function showDescription(label) {
-//   const descriptions = {
-//     "Diagnostics": "Using AI to identify diseases early.",
-//     "Treatment": "AI-assisted personalized treatment plans.",
-//     "Medical Imaging": "AI for analyzing X-rays and MRIs.",
-//     "Drug Discovery": "AI helps find new potential medicines.",
-//     "Monitoring": "Real-time health monitoring with AI.",
-//     "Privacy": "Protecting patient data confidentiality.",
-//     "Bias": "Avoiding unfair treatment due to skewed data.",
-//     "Transparency": "Making AI decisions explainable.",
-//     "Accountability": "Ensuring responsibility in AI use.",
-//     "Data Quality": "Ensuring input data is accurate and complete.",
-//     "Model Robustness": "Making AI perform well across scenarios.",
-//     "Scalability": "Expanding AI systems efficiently.",
-//     "Integration": "Merging AI tools with hospital systems.",
-//     "Security": "Preventing attacks on healthcare AI.",
-//     "Explainability": "Understanding how AI makes decisions."
-//   };
-
-//   const descriptionText = descriptions[label] || "No description available.";
-
-//   // Update both boxes if present
-//   const appBox = document.getElementById("applicationsDescription");
-//   const chalBox = document.getElementById("challengesDescription");
-//   const tchalBox = document.getElementById("technicalchallengesDescription");
-
-//   if (appBox) appBox.innerHTML = `<p>${descriptionText}</p>`;
-//   if (chalBox) chalBox.innerHTML = `<p>${descriptionText}</p>`;
-//   if (tchalBox) tchalBox.innerHTML = `<p>${descriptionText}</p>`;
-// }
 function showDescription(label, boxId) {
   const descriptions = {
     "Enable Independence": "Helping with daily tasks, such as schedule appointments, manage medication reminders, or navigate online services. <br>Helping with basic tasks such as grocery ordering, appliances control, etc",
@@ -44,8 +14,8 @@ function showDescription(label, boxId) {
     "Integration": "Merging AI tools with hospital systems.",
     "Fairness": "Desription text will appear here.",
     "Accountability": "Desription text will appear here.",
-    "Data Management": "Desription text will appear here.",
-
+    "Data Management": "Robust measures needed to comply HIPAA and GDPR in data handling. <br>Do the patients retain data owndership?"
+    "Prompt Injection": "Prompt injection can override safeguards and sysem prompts. <br>Adversary triggers jail-broken LLM to reveal private medical history or provide unsafe suggestions ",
   };
 
   const descriptionText = descriptions[label] || "No description available.";
